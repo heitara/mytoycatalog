9197099d02e38a65275d12a993960181752480de
Emil Atanasov <heitara@gmail.com>
2024-10-15 09:44:56 +0300

    Initial Commit

---
+++ MyCatalog/MyCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCatalogApp.swift
+//  MyCatalog
+//
+//  Created by Emil Atanasov on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCatalogTests/MyCatalogTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCatalogTests.swift
+//  MyCatalogTests
+//
+//  Created by Emil Atanasov on 10/15/24.
+//
+
+import Testing
+@testable import MyCatalog
+
+struct MyCatalogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
